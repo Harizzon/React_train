f7075c892a61661e7f222f18718efb3715ef8148
Harizzon <dmitriypodgora@gmail.com>
2021-01-16 12:47:27 +0200

    Created repo for the first task

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// console.log(1);
+
+// let number = 5;
+// const userName = 'Tom';
+
+// console.log(number);
+// console.log(userName);
+// number = 10;
+// console.log(number);
+
+
+// alert('We have a one question');
+// let gay1 = confirm('Are you a gay');
+
+let info = [];
+
+// info[0] = prompt('Why are you gay?');
+// info[1] = prompt('Suck some ...?');
+// info[2] = +prompt('Your age');
+
+console.log(typeof(info));
