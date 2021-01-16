e363bafd7f04bbe5e33c8ae39dd1637bb723b374
Harizzon <dmitriypodgora@gmail.com>
2021-01-16 13:07:49 +0200

    added comleted 1st task

--- js/script.js
+++ js/script.js
@@ -1,23 +1,16 @@
 "use strict";
 
-// console.log(1);
+let numberOfFilms = Number(prompt("How many films you have watched? ", ''));
 
-// let number = 5;
-// const userName = 'Tom';
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actor: {},
+    genres: {},
+    privat: false
+};
 
-// console.log(number);
-// console.log(userName);
-// number = 10;
-// console.log(number);
+let fQuestion = prompt('What last movie you saw?', ''),
+    sQuestion = +prompt("Mark", '');
 
-
-// alert('We have a one question');
-// let gay1 = confirm('Are you a gay');
-
-let info = [];
-
-// info[0] = prompt('Why are you gay?');
-// info[1] = prompt('Suck some ...?');
-// info[2] = +prompt('Your age');
-
-console.log(typeof(info));
+personalMovieDB.movies[fQuestion] = sQuestion;
